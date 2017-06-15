4e178bf0bf92dc435ca507b5c108dd3b8d295c1e
Breno Arosa <breno.arosa@twistsystems.com>
2017-06-15 17:15:08 -0300

    Adds mutual info

--- ica.py
+++ ica.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from glob import glob
 from sklearn.decomposition import FastICA
+from sklearn.metrics import normalized_mutual_info_score
 import os
 
 
@@ -19,39 +20,80 @@ def save_audio(x, sample_rate=44100, filepath='audio.wav'):
     wavfile.write(filepath, sr, x_o)
 
 
-SOURCE_GLOB = "mono/*.wav"
+def mutual_info_pairwise(matrix, normalize=True):
+    """
+    Calculates mutual information between matrix columns.
+    Normalization is required to valid results.
+
+    If normalize parameters is True, mormalize matrix to 16-bits integer.
+    Scaling it to be 1/2 of it's dynamic range.
+    """
+    size = matrix.shape[1]
+
+    if normalize:
+        factor = 2.**14/np.max(np.abs(matrix), axis=0) * np.eye(size)
+        matrix = np.dot(matrix, factor).astype('int16')
+
+    mi_matrix = np.zeros(shape=(size, size), dtype="float64")
+    for i in range(size):
+        for j in range(i, size):
+            mi = normalized_mutual_info_score(matrix[:, i], matrix[:, j])
+            mi_matrix[i, j] = mi
+            mi_matrix[j, i] = mi
+
+    return mi_matrix
+
+
+SOURCE_GLOB = "voices/*.wav"
 SOURCES_COUNT = len(glob(SOURCE_GLOB))
 MICROPHONE_COUNT = SOURCES_COUNT
-audio_len = int(6e6) # 6M samples
+audio_len = int(5e5) # .5M samples
 
 if not os.path.exists('output'):
     os.makedirs('output')
 
 assert(MICROPHONE_COUNT >= SOURCES_COUNT)
 
+
 # Source matrix
-S = np.zeros((audio_len, SOURCES_COUNT))
+S = np.zeros(shape=(audio_len, SOURCES_COUNT), dtype='int16')
 for i, filename in enumerate(glob(SOURCE_GLOB)):
     sr, s = wavfile.read(filename)
     S[:, i] = s[0:audio_len]
 
-
 # Mixing matrix
 A = np.random.rand(MICROPHONE_COUNT, SOURCES_COUNT)
+A = (A * .8) + .1 # limits from .1 to .9
 print('Mixing matrix: \n{}'.format(A))
 
 X = np.dot(S, A.T)  # Generate observations
-
-for i in range(X.shape[1]):
-    save_audio(X[:, i], sr, 'output/mixed_{}.wav'.format(i))
 
 # Compute ICA
 ica = FastICA(n_components=SOURCES_COUNT)
 S_ = ica.fit_transform(X)  # Reconstruct signals
 A_ = ica.mixing_  # Get estimated mixing matrix
 
+
+# Saving mixed and recovered sources to disk
+for i in range(X.shape[1]):
+    save_audio(X[:, i], sr, 'output/mixed_{}.wav'.format(i))
+
 for i in range(S_.shape[1]):
     save_audio(S_[:, i], sr, 'output/{}.wav'.format(i))
+
+
+# MI matrixs
+mi_S = mutual_info_pairwise(S)
+print("Sources Mutual Info:")
+print(mi_S)
+
+mi_X = mutual_info_pairwise(X)
+print("Microphones Mutual Info:")
+print(mi_X)
+
+mi_S_ = mutual_info_pairwise(S_)
+print("Reconstructed Sources Mutual Info:")
+print(mi_S_)
 
 
 """
